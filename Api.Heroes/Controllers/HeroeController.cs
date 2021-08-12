61bf26632b2193787effd5b1f259bf7632d2e30c
Furguson <furguson@email.com>
2021-08-13 00:11:08 +0200

    Agregar archivos de proyecto.

---
+++ Api.Heroes/Controllers/HeroeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Heroes.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Heroes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HeroeController : ControllerBase
+    {
+        private readonly TESTContext _context;
+
+        public HeroeController(TESTContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Get all heroes
+        /// </summary>
+        [HttpGet]
+        public ActionResult<List<Heroe>> Get()
+        {
+            var heroes = _context.Heroes.ToList();
+            return heroes;
+        }
+    }
+}
---
+++ Api.Heroes/Data/TESTContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Heroes.Data
+{
+    public class TESTContext : DbContext
+    {
+        public TESTContext(DbContextOptions<TESTContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Heroe> Heroes { get; set; }
+    }
+}
